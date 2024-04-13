dd259a9d7605feade0a03d40f46ec42c0a20562b
Diego Rojas <diego.rojas@millicom.com>
2024-04-13 19:07:24 +1000

    Some test cases have been included. Aditionally some comments have been deleted

--- internal/helpers/helper_test.go
+++ internal/helpers/helper_test.go
@@ -9,16 +9,43 @@ func TestPagination(t *testing.T) {
 
 	testCases := []struct {
 		Name string
+		input []string
+		items_per_page int
+		paginationExpetedAfterInit int
+		
+
 	}{
 		{
 			Name: "test1",
+			input: []string{"a", "b", "c", "d", "e", "f","g","h","i","j"},
+			items_per_page: 3,
+			paginationExpetedAfterInit: 4,
+			
+		},
+		{
+			Name: "test1",
+			input: []string{"a", "b", "c", "d", "e", "f","g","h","i","j"},
+			items_per_page: 2,
+			paginationExpetedAfterInit: 5,
+		},
+		{
+			Name: "test1",
+			input: []string{"a", "b", "c", "d", "e", "f","g","h","i","j"},
+			items_per_page: 1,
+			paginationExpetedAfterInit: 10,
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
+			//t.Parallel()
 			fmt.Println("This is the testing for the testcase:", tc.Name, ".")
 			var testedStructure PaginationHelper
-			testedStructure.Init([]string{"a", "b", "c", "d", "e", "f","g","h","i","j"}, 3)
+			testedStructure.Init(tc.input, tc.items_per_page)
+			if tc.paginationExpetedAfterInit!= len(testedStructure.selfColection){
+				t.Errorf("Pagination unsucessful from Init fuction. The pagination resulted is %d, and the pagination expected is %d",
+				len(testedStructure.selfColection),tc.paginationExpetedAfterInit)
+				t.Fail()
+			}
 			pages := testedStructure.Page_count()              //should return 2
 			items := testedStructure.Item_count()              //should return 6
 			itemsByPage := testedStructure.Page_item_count(0)  //should return 4
--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -27,41 +27,21 @@ type PaginationHelper struct {
 //In golang it means slide of elements
 
 func (r *PaginationHelper) Init(colection []string, items_per_page int) {
-	//[0,1,2,3,4,5,6,7,8,9] -> 3 => [[0,1,2],[3,4,5],[6,7,8],[9]]
-	//while the array has more than 0 elements, iteration is going.
-	//iteration in the original array every x items_per_page
-	//size=10 and items_per_page=3 --> 3 and extra 1
-	// index+(iteration)*(items_per_page)
-	// (0,1,2,3,4,5,6,7,8,9)+(0,1,2)*3
-	// [initial:end]=[0:2][3:5][6:8][9::]
-	//[(iteration)*(items_per_page):((iteration+1)*(items_per_page))-1]
-	//[(0)*(3):(1)*(3)]= [0:3]
-	//[(1)*(3):(2)*(3)]= [3:6]
-	//[(2)*(3):(3)*(3)]= [6:9]
-	//((iteration+1)*(items_per_page))-1
-	//((x+1)(y))-1
-	//xy+y-1////////////
-	//(xy+y)-1
-	//(y(x+1))-1
-	//[(it*ipp):(ipp+ipp*it-1)]///////
 	ipp := items_per_page
-	for it := 0; it < int(len(colection)/items_per_page); it++ {
-		subArray := colection[(it * ipp):(ipp + ipp*it)] //0:2//3:5//6:8//9:11
+	//Adding each complete slice to the array 
+	for it := 0; it < int(len(colection)/ipp); it++ {
+		subArray := colection[(it * ipp):(ipp + ipp*it)]
 		r.selfColection = append(r.selfColection, subArray)
 	}
-	// len(colection)%items_per_page = 1
-	// if mod>0 => [len(colection)-mod::]
-	//[10-1::]->[9::]
-	//[11-2::]->[9::]
-	//[12-3::]->[9::]
-	mod:=len(colection)%items_per_page
-	if mod >0{
-		subArray:= colection[(len(colection)-mod):]
+	//adding the last array
+	mod := len(colection) % ipp
+	if mod > 0 {
+		subArray := colection[(len(colection) - mod):]
 		r.selfColection = append(r.selfColection, subArray)
 	}
-
 }
 func (r *PaginationHelper) Page_count() int {
+
 	return 5
 }
 func (r *PaginationHelper) Item_count() int {
